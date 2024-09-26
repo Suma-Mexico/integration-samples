65cb216a1accbb17e91521bc11e766274af61672
Charly MI <carlos.montes@sumamexico.com>
2024-09-26 12:51:32 -0600

    feat(constants): update credentials and endpoints

--- app/constants/credentials.ts
+++ app/constants/credentials.ts
@@ -1,5 +1,6 @@
 const client_id = "YOUR_CREDENTIALS";
 const client_secret = "YOUR_CREDENTIALS";
 const publicApiKey = "YOUR_CREDENTIALS";
+const privateApiKey = "YOUR_CREDENTIALS";
 
-export { client_id, client_secret, publicApiKey };
+export { client_id, client_secret, publicApiKey, privateApiKey };
--- app/constants/endpoints.ts
+++ app/constants/endpoints.ts
@@ -1,3 +1,4 @@
 const API_ID_V3 = "https://veridocid.azure-api.net/api/id/v3";
+const API_URL = "https://veridocid.azure-api.net/api";
 
-export { API_ID_V3 };
+export { API_ID_V3, API_URL };
