ebe38d8e121930a249acb32f43e38c4ba5dd9b92
Charly MI <carlos.montes@sumamexico.com>
2024-09-26 11:40:19 -0600

    feat(credentials.ts): add public api key

--- app/constants/credentials.ts
+++ app/constants/credentials.ts
@@ -1,4 +1,5 @@
 const client_id = "YOUR_CREDENTIALS";
 const client_secret = "YOUR_CREDENTIALS";
+const publicApiKey = "YOUR_CREDENTIALS";
 
-export { client_id, client_secret };
+export { client_id, client_secret, publicApiKey };
