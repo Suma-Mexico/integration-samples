9766b78c6e7444141e47b0f2adf695c4e4556557
Charly MI <charly_1310@outlook.com>
2024-09-26 11:33:29 -0600

    feat(constants): add credentials and endpoints

---
+++ app/constants/credentials.ts
@@ -0,0 +1,4 @@
+const client_id = "YOUR_CREDENTIALS";
+const client_secret = "YOUR_CREDENTIALS";
+
+export { client_id, client_secret };
---
+++ app/constants/endpoints.ts
@@ -0,0 +1,3 @@
+const API_ID_V3 = "https://veridocid.azure-api.net/api/id/v3";
+
+export { API_ID_V3 };
